f4639e6f6186616afc3c53c5b9fcad4c6d05b8d0
mnpkoerner <mnpkoerner@gmail.com>
2021-04-14 14:24:38 -0500

    username and password credential bundled and posting to server. server error 500

--- client/codex-ui/src/components/App/App.js
+++ client/codex-ui/src/components/App/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Spike />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <Spike />
+
     </div>
   );
 }
--- client/codex-ui/src/components/Spike/Spike.js
+++ client/codex-ui/src/components/Spike/Spike.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
+import {useState} from 'react'
+
 
 export default function Spike() {
 
-    const spike = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const spike = (event) => {
+        event.preventDefault()
         console.log('connected')
-        axios.get('/api/user').then((response)=>{
+        const credential = {
+            username: username,
+            password: password,
+        }
+        console.log(credential)
+        axios.post('/', credential).then((response)=>{
             console.log(response)
         }).catch((error)=>{
             console.log(error)
@@ -13,8 +24,22 @@ export default function Spike() {
 
     return(
         <div>
-            <h2>Button To Test:</h2>
-            <button onClick={()=>spike()}>Send HTTP</button>
+            <h2>Login:</h2>
+            <form>
+                <input
+                type="text"
+                value={username}
+                placeholder="username"
+                onChange={(event)=>setUsername(event.target.value)}>
+                </input>
+                <input
+                type="text"
+                value={password}
+                placeholder="password"
+                onChange={(event)=>setPassword(event.target.value)}>
+                </input>
+            </form>
+            <button type="submit" onClick={(event)=>spike(event)}>Send HTTP</button>
         </div>
     )
 }

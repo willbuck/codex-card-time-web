b0addf1bae5bdab2f9d1e2e87fd68af8c3391c45
mnpkoerner <mnpkoerner@gmail.com>
2021-04-14 14:10:30 -0500

    axios Spike logs server response. need to update URL

--- client/codex-ui/src/components/Spike/Spike.js
+++ client/codex-ui/src/components/Spike/Spike.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 export default function Spike() {
 
     const spike = () => {
         console.log('connected')
+        axios.get('/api/user').then((response)=>{
+            console.log(response)
+        }).catch((error)=>{
+            console.log(error)
+        })
     }
-    
+
     return(
         <div>
             <h2>Button To Test:</h2>
